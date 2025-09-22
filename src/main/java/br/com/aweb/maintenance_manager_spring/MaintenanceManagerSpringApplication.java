0957d15a66daf8ecfff95e689711a7bb34b3bb87
CaioDalNegro <caiodalnegro@gmail.com>
2025-09-22 07:53:14 -0300

    first commit

---
+++ src/main/java/br/com/aweb/maintenance_manager_spring/MaintenanceManagerSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.aweb.maintenance_manager_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceManagerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceManagerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aweb/maintenance_manager_spring/MaintenanceManagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aweb.maintenance_manager_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceManagerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
